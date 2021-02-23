7e15c2e3126aa97188d3aa6b015fcda2da67dcf0
MrShadowalker <mr.shadowalker@foxmail.com>
2021-02-23 11:13:55 +0800

    reformat

--- miaosha-dao/src/main/java/cn/monitor4all/miaoshadao/utils/CacheKey.java
+++ miaosha-dao/src/main/java/cn/monitor4all/miaoshadao/utils/CacheKey.java
@@ -2,14 +2,14 @@ package cn.monitor4all.miaoshadao.utils;
 
 public enum CacheKey {
 
-    HASH_KEY("miaosha_v1_user_hash"),
-    LIMIT_KEY("miaosha_v1_user_limit"),
-    STOCK_COUNT("miaosha_v1_stock_count"),
-    USER_HAS_ORDER("miaosha_v1_user_has_order");
+    HASH_KEY("seckill_v1_user_hash"),
+    LIMIT_KEY("seckill_v1_user_limit"),
+    STOCK_COUNT("seckill_v1_stock_count"),
+    USER_HAS_ORDER("seckill_v1_user_has_order");
 
-    private String key;
+    private final String key;
 
-    private CacheKey(String key) {
+    CacheKey(String key) {
         this.key = key;
     }
 
--- miaosha-web/src/main/java/cn/monitor4all/miaoshaweb/controller/OrderController.java
+++ miaosha-web/src/main/java/cn/monitor4all/miaoshaweb/controller/OrderController.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -125,7 +124,7 @@ public class OrderController {
      *
      * @return
      */
-    @RequestMapping(value = "/getVerifyHash", method = {RequestMethod.GET})
+    @GetMapping(value = "/getVerifyHash")
     @ResponseBody
     public String getVerifyHash(@RequestParam(value = "sid") Integer sid,
                                 @RequestParam(value = "userId") Integer userId) {
